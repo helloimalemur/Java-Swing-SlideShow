e06a9e3cfa765bc113f27fb35dbe2ae16ed65749
foxx <jkoontsiii@gmail.com>
2022-07-09 23:38:34 -0400

    prog

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,20 @@
 import javax.swing.*;
-import java.awt.FlowLayout;
+import java.awt.*;
 import java.util.*;
 import java.nio.*;
+import java.util.List;
 import java.util.Timer;
 
 class SlideShow extends JFrame {
+    public static JPanel panel = new JPanel(new FlowLayout());
+    public static Label label = new Label();
+
     public SlideShow() {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300,300);
         setVisible(true);
-        setLayout(new FlowLayout());
+        FlowLayout layout = new FlowLayout();
+        setLayout(layout);
         List<String> used = new ArrayList<>();
         List<String> using = new ArrayList<>();
         List<String> tused = new ArrayList<>();
@@ -17,6 +22,10 @@ class SlideShow extends JFrame {
         Timer timer = new Timer();
         TimerTask task = new TimerHelper();
         timer.schedule(task, 1000, 1000);
+        add(panel);
+        panel.setVisible(true);
+        label.setText("not null");
+        label.setVisible(true);
     }
 }
 
@@ -29,9 +38,15 @@ class Meth {
 }
 
 class TimerHelper extends TimerTask {
+    public int counter = 0;
     public void run() {
         Meth x = new Meth();
         x.LoadImages();
+        counter++;
+        System.out.println(counter);
+        SlideShow.panel.add(SlideShow.label);
+
+        SlideShow.label.setSize(counter, counter);
     }
 }
 
