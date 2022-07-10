e7b51d01e4e1ff2e54af7950f8412e86ae1de926
foxx <jkoontsiii@gmail.com>
2022-07-09 21:29:04 -0400

    port to JAVA

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,8 @@
-import javax.imageio.stream.ImageInputStream;
 import javax.swing.*;
-import javax.swing.plaf.basic.BasicComboBoxUI;
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.List;
-import java.io.IOException;
-import java.nio.file.DirectoryStream;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
+import java.awt.FlowLayout;
+import java.util.*;
+import java.nio.*;
+import java.util.Timer;
 
 class SlideShow extends JFrame {
     public SlideShow() {
@@ -20,31 +10,30 @@ class SlideShow extends JFrame {
         setSize(300,300);
         setVisible(true);
         setLayout(new FlowLayout());
-
         List<String> used = new ArrayList<>();
         List<String> using = new ArrayList<>();
         List<String> tused = new ArrayList<>();
         List<String> pused = new ArrayList<>();
-        List<String> x = FileContentReader.main();
-
-
-        System.out.println(x);
+        Timer timer = new Timer();
+        TimerTask task = new TimerHelper();
+        timer.schedule(task, 1000, 1000);
     }
-/*
-    public GetImages(List paths) {
-        public File paths[] = new File();
-
-    }
-*/
-
-    /*loads images */
-     public void LoadImages() {
-
-    }
-
-
 }
 
+class Meth {
+    /*loads images */
+    public void LoadImages() {
+        List<String> x = FileContentReader.main();
+        System.out.println(x.toString().length());
+    }
+}
+
+class TimerHelper extends TimerTask {
+    public void run() {
+        Meth x = new Meth();
+        x.LoadImages();
+    }
+}
 
 
 class Main{
