bf31a40c48882dad06e70fa4bf205d663ec10d46
foxx <jkoontsiii@gmail.com>
2022-09-29 12:03:05 -0400

    working on overlay time threshold

--- src/main/java/SlideShow/SlideShow.java
+++ src/main/java/SlideShow/SlideShow.java
@@ -16,6 +16,10 @@ class SlideShow extends JFrame {
     public static Slides slides; //initialize net.koonts.slideshow.Slides class
 
 
+    public Label overlaylabel = new Label();
+    public JPanel overlayPanel = new JPanel(new FlowLayout());
+
+
 
     public SlideShow(String paths) {
         slides = new Slides(paths);
@@ -40,13 +44,45 @@ class SlideShow extends JFrame {
         start();
     }
 
+    public void overlay(String overlaystring, long overlaylong) {
+        try {
+            overlayPanel.remove(overlaylabel);
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        overlaylabel.setText(overlaystring + String.valueOf(overlaylong));
+        overlaylabel.setSize(300,100);
+        overlaylabel.setLocation(0,0);
+
+        overlaylabel.setBackground(Color.white);
+        overlaylabel.setForeground(Color.black);
+        overlaylabel.setVisible(true);
+        overlayPanel.setVisible(true);
+
+        overlayPanel.add(overlaylabel);
+        try {
+            slides.panel.add(overlayPanel);
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        try {
+            Thread.sleep(1000);
+            overlayPanel.remove(overlaylabel);
+            slides.panel.remove(overlayPanel);
+        } catch (Exception exception) {
+            System.out.println(exception);
+        }
+        overlaylabel.setVisible(false);
+        overlayPanel.setVisible(false);
+    }
+
 
     public void start() {
         while (!(slides == null)) {
             slides.pause = false;
             slides.waspaused = false;
             slides.next = false;
-            slides.panel.removeAll();
+            overlayPanel.removeAll();
             slides.loadImageGlob(); //gets files glob and sets 'listofimages'
             slides.cycleImages(slides.listofimages);
         }
@@ -73,14 +109,14 @@ class SlideShow extends JFrame {
             if (e.getKeyChar() == 'a') { // a key increases interval between slides
                 slides.slideinterval = slides.slideinterval + 3000;
                 System.out.println("interval: " + slides.slideinterval);
-                slides.overlay("Delay: ", slides.slideinterval);
+                overlay("Delay: ", slides.slideinterval);
             }
 
             if (e.getKeyChar() == 'z') { // z key decreased interval between slides
                 if (slides.slideinterval >= 3000) {
                     slides.slideinterval = slides.slideinterval - 2000;
                     System.out.println("interval: " + slides.slideinterval);
-                    slides.overlay("Delay: ", slides.slideinterval);
+                    overlay("Delay: ", slides.slideinterval);
                 }
             }
 
